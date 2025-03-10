713b92680fa2cf2dec6744160f7147fb2fa40025
Lina Fowler <be.the.boat@gmail.com>
2025-03-10 01:15:20 -0400

    up

---
+++ js/main.js
@@ -0,0 +1,52 @@
+const makeRequest = async (text) => {
+    try {
+        const response = await fetch("http://35.193.238.101:5001/query", {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ prompt: text })
+        });
+        if (!response.ok) {
+            throw new Error('Network response was not ok');
+        }
+        const data = await response.json();
+        return data;
+    } catch (error) {
+        console.error('There has been a problem with your fetch operation:', error);
+    }
+};
+
+const setup = () => {
+    const form = document.querySelector('#chat-form');
+    const input = document.querySelector('.chat-input');
+    form.addEventListener('submit', async (e) => {
+        e.preventDefault();
+        const text = input.value;
+        document.querySelector('#chat-response').textContent = "Loading...";
+        const data = await makeRequest(text);
+        const paragraph = document.querySelector('#chat-response');
+        const { answer, project } = data;
+        const regex = /\[([^\]]+)\]/g;
+        const updatedText = answer.replace(regex, (match, p1) => {
+            return `<a href="https://linafowler.com/pages/${project}"}" target="_blank">${p1}</a>`;
+        });
+        paragraph.innerHTML = updatedText;
+    });
+};
+
+const hideModal = () => {
+    const shadow = document.querySelector(".shadow");
+    shadow.classList.add("hidden");
+    const modal = document.querySelector(".chat-modal");
+    modal.classList.add("hidden");
+}
+
+const showModal = () => {
+    const shadow = document.querySelector(".shadow");
+    shadow.classList.remove("hidden");
+    const modal = document.querySelector(".chat-modal");
+    modal.classList.remove("hidden");
+}
+
+document.addEventListener('DOMContentLoaded', setup);
